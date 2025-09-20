fadb1d6dc9b6bf00fe4a00e1282b2fb11ef1e47f
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-09-21 02:09:55 +0300

    Add Noise

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, random
 from collections.abc import Callable
 from PIL import Image, ImageFile
 
@@ -41,6 +41,25 @@ def negative(image: ImageFile.ImageFile) -> Image.Image:
             processed_image.putpixel((x, y), (255 - r, 255 - g, 255 - b))
     return processed_image
 
+def noise(image: ImageFile.ImageFile) -> Image.Image:
+    FACTOR = 60
+
+    processed_image = Image.new("RGB", image.size)
+    (width, height) = image.size
+    for x in range(width):
+        for y in range(height):
+            (r, g, b) = image.getpixel((x, y))
+
+            rand = random.randint(-FACTOR, FACTOR)
+            color = [r + rand, g + rand, b + rand]
+            for index, gradient in enumerate(color):
+                if gradient < 0:
+                    color[index] = 0
+                elif gradient > 255:
+                    color[index] = 255
+            processed_image.putpixel((x, y), tuple(color))
+    return processed_image
+
 def main(transform: Callable[[ImageFile.ImageFile], Image.Image]):
     shrek_image = os.path.join(RESOURCES_DIR, "shrek.jpg")
     with Image.open(shrek_image) as image:
@@ -50,8 +69,8 @@ def main(transform: Callable[[ImageFile.ImageFile], Image.Image]):
 
 
 if __name__ == "__main__":
-    transforms = (shades_of_gray, sepia, negative)
+    transforms = (shades_of_gray, sepia, negative, noise)
     try:
-        main(transforms[2])
+        main(transforms[3])
     except Exception as exception:
         print(f"Error occured: {exception}", file=sys.stderr)
