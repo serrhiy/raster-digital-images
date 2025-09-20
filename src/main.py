5933bb99c628b7e0c8e273079e5eaffd1035f35b
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-09-21 02:19:15 +0300

    Add Monochrome

--- src/main.py
+++ src/main.py
@@ -83,6 +83,19 @@ def brightness_change(image: ImageFile.ImageFile) -> Image.Image:
     return processed_image
 
 
+def monochrome(image: ImageFile.ImageFile) -> Image.Image:
+    processed_image = Image.new("RGB", image.size)
+    (width, height) = image.size
+    for x in range(width):
+        for y in range(height):
+            (r, g, b) = image.getpixel((x, y))
+            avg = (r + g + b) // 3
+            color = 255 if avg > 255 // 2 else 0
+            processed_image.putpixel((x, y), (color, color, color))
+
+    return processed_image
+
+
 def main(transform: Callable[[ImageFile.ImageFile], Image.Image]):
     shrek_image = os.path.join(RESOURCES_DIR, "shrek.jpg")
     with Image.open(shrek_image) as image:
@@ -92,8 +105,8 @@ def main(transform: Callable[[ImageFile.ImageFile], Image.Image]):
 
 
 if __name__ == "__main__":
-    transforms = (shades_of_gray, sepia, negative, noise, brightness_change)
+    transforms = (shades_of_gray, sepia, negative, noise, brightness_change, monochrome)
     try:
-        main(transforms[4])
+        main(transforms[5])
     except Exception as exception:
         print(f"Error occured: {exception}", file=sys.stderr)
