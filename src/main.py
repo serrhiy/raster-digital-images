c5633e08eddce4c27e3f7422d4e2ac6698d31971
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-09-21 03:16:47 +0300

    Add Detail

--- src/main.py
+++ src/main.py
@@ -92,7 +92,42 @@ def monochrome(image: ImageFile.ImageFile) -> Image.Image:
             avg = (r + g + b) // 3
             color = 255 if avg > 255 // 2 else 0
             processed_image.putpixel((x, y), (color, color, color))
+    return processed_image
 
+
+def detail(image: ImageFile.ImageFile) -> Image.Image:
+    # fmt: off
+    DETAIL_FILTER = (
+        0,  -1,  0,
+        -1, 5, -1,
+        0,  -1,  0,
+    )
+    # fmt: on
+    processed_image = Image.new("RGB", image.size)
+    (width, height) = image.size
+    for x in range(1, width - 1):
+        for y in range(1, height - 1):
+            pixels = []
+            for y_position in range(y - 1, y + 1 + 1):
+                for x_position in range(x - 1, x + 1 + 1):
+                    pixel = image.getpixel((x_position, y_position))
+                    pixels.append(pixel)
+
+            color = []
+            for color_index in range(3):
+                gradient = 0
+                for pixel_index in range(len(DETAIL_FILTER)):
+                    gradient += (
+                        pixels[pixel_index][color_index] * DETAIL_FILTER[pixel_index]
+                    )
+                if gradient < 0:
+                    gradient = 0
+                elif gradient > 255:
+                    gradient = 255
+                color.append(gradient)
+            processed_image.putpixel((x, y), tuple(color))
+
+    # return image.copy().filter(DETAIL)
     return processed_image
 
 
@@ -105,8 +140,16 @@ def main(transform: Callable[[ImageFile.ImageFile], Image.Image]):
 
 
 if __name__ == "__main__":
-    transforms = (shades_of_gray, sepia, negative, noise, brightness_change, monochrome)
+    transforms = (
+        shades_of_gray,
+        sepia,
+        negative,
+        noise,
+        brightness_change,
+        monochrome,
+        detail,
+    )
     try:
-        main(transforms[5])
+        main(transforms[6])
     except Exception as exception:
         print(f"Error occured: {exception}", file=sys.stderr)
